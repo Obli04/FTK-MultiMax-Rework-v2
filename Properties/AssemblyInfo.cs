d1fd95ef63d16a976d54d033ffe10eabab099647
PolarsBear <63355054+PolarsBear@users.noreply.github.com>
2025-06-22 18:52:25 -0300

    Remove dashes in assembly name

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FTK-MultiMax-Rework-v2")]
+[assembly: AssemblyTitle("FTK MultiMax Rework v2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Polars Bear (upon work by justedm & samupo)")]
-[assembly: AssemblyProduct("FTK-MultiMax-Rework-v2")]
+[assembly: AssemblyProduct("FTK MultiMax Rework v2")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
